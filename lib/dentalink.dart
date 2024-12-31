00f8921a5dc66b07b11e348d01c9a972cd4f63fc
tasneem-gamal <tasneemgamal044@gmail.com>
2024-12-31 10:50:28 -0800

    add screenutil

--- lib/dentalink.dart
+++ lib/dentalink.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class Dentalink extends StatelessWidget {
   const Dentalink({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return ScreenUtilInit(
+      designSize: ScreenUtil.defaultSize,
+      minTextAdapt: true,
+      child: MaterialApp(
+        title: 'DentaLink',
+        theme: ThemeData(),
+        debugShowCheckedModeBanner: false,
+      )
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:dentalink/dentalink.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  ScreenUtil.ensureScreenSize;
   runApp(const Dentalink());
 }

5ba8c14c0acca4aa73a012afe7ff3bce9bd6d77c
tasneem-gamal <tasneemgamal044@gmail.com>
2025-01-27 13:22:10 -0800

    solve issues and add assets

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,8 @@ import 'package:dentalink/dentalink.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  ScreenUtil.ensureScreenSize;
+  await ScreenUtil.ensureScreenSize();
   runApp(Dentalink(appRouter: AppRouter(),));
 }

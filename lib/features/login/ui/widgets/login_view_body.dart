324321d67745c1346521863e79ec2e159ca0985e
tasneem-gamal <tasneemgamal044@gmail.com>
2025-02-06 12:07:53 -0800

    refactor login

--- lib/features/login/ui/widgets/login_view_body.dart
+++ lib/features/login/ui/widgets/login_view_body.dart
@@ -3,9 +3,7 @@ import 'package:dentalink/core/helpers/extension.dart';
 import 'package:dentalink/core/helpers/spacing.dart';
 import 'package:dentalink/core/widgets/have_account_text.dart';
 import 'package:dentalink/core/widgets/head_welcome_text_auth.dart';
-import 'package:dentalink/core/widgets/other_login_row_options.dart';
 import 'package:dentalink/features/login/ui/widgets/login_form.dart';
-import 'package:dentalink/features/login/ui/widgets/other_login_options_divider.dart';
 import 'package:flutter/material.dart';
 
 class LoginViewBody extends StatelessWidget {
@@ -25,13 +23,7 @@ class LoginViewBody extends StatelessWidget {
             ),
             verticalSpace(48),
             const LoginForm(),
-            verticalSpace(30),
-            const OtherLoginOptionsDivider(),
             verticalSpace(24),
-            const OtherLoginRowOptions(
-              width: 169,
-            ),
-            verticalSpace(14),
             HaveAccountText(
               title: "Don’t have an account? ", 
               navTitle: "Sign up.", 
